acf8b21964e9d2ed10473b124a8e17a15e5e4b4b
1606yash <yash.goyal@softude.com>
2026-03-02 18:03:50 +0530

    rename project name

---
+++ commercium/hooks.py
@@ -0,0 +1,15 @@
+app_name = "commercium"
+app_title = "Commercium"
+app_publisher = "Shubham Bajpayee"
+app_description = "OAuth for Multiple Sites"
+app_email = "help@mycommercium.com"
+app_license = "MIT"
+app_version = "0.0.1"
+
+# Fixtures for OAuth Client
+fixtures = [
+    {
+        "dt": "OAuth Client",
+        "filters": [["app_name", "=", "Commercium"]]
+    }
+]
